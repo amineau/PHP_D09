63cb516c33f6a5cbe079385102711edec228e920
mineau <antoine.mineau@hotmail.fr>
2016-04-16 17:29:23 +0000

    first

---
+++ ex02bis/todo.js
@@ -0,0 +1,77 @@
+var index = maxId();
+
+function addList()
+{
+    var text = prompt();
+    var idName = 'child' + index;
+    newList(idName, text);
+}
+
+function newList(idName, text)
+{
+    var newElement = document.createElement("div");
+    var myDiv = document.getElementById('ft_list');
+    var prev = index - 1
+    
+    console.log(text);
+    if (text != "" && text)
+    {
+        newElement.setAttribute('id', idName);
+        newElement.setAttribute('onclick', 'deleted('+ idName + ')');
+        newElement.appendChild(document.createTextNode(text));
+        if (!myDiv.childNodes[0])
+            myDiv.appendChild(newElement);
+        else
+            myDiv.insertBefore(newElement, myDiv.childNodes[0]);
+        console.log('['+idName+']');
+        createCookie(idName, text, 1);
+        index++;
+    }
+    console.log(document.cookie);
+}
+
+function deleted(oldChild)
+{
+    var myDiv = document.getElementById('ft_list');
+    if (confirm("Are you sure you want to delete this item ?")){
+        eraseCookie(oldChild.getAttribute('id'));
+        myDiv.removeChild(oldChild);
+    }
+}
+
+function createCookie(name, value, day)
+{
+	var date = new Date();
+	date.setTime(date.getTime()+(day*24*60*60*1000));
+	var expires = "; expires="+ date.toGMTString();
+	document.cookie = name + "=" + value + expires+"; path=/";
+}
+
+function checkCookie() {
+	var cook = document.cookie.split(';');
+	var c;
+	
+	for(var i = 0; i < cook.length; i++) {
+		c = cook[i];
+		while (c.charAt(0) == ' ')
+		    c = c.substring(1,c.length);
+		newList(c.substring(0, c.indexOf('=')), c.substring(c.indexOf('=') + 1, c.length));
+	}
+}
+
+function maxId()
+{
+    var max = 0;
+    var cook = document.cookie.split(';');
+    
+    for(var i = 0; i < cook.length; i++) {
+        var idValue = parseInt(cook[i].charAt(cook[i].indexOf('=') - 1));
+        if (max < idValue)
+            max = idValue;
+    }
+    return max + 1;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
